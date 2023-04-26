5c0ad8d308022362ce1656ecf470429674584fb3
daniellecall2005 <124648834+daniellecall2005@users.noreply.github.com>
2023-04-25 22:26:50 -0600

    Forecast Days Added

--- src/script.js
+++ src/script.js
@@ -24,29 +24,31 @@ function formatDate(date) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  forecastElement.innerHTML = "";
-  <div class="weather-forecast" id="forecast">
-  <div class="row">
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
     <div class="col-2">
-      <div class="weather-forecast-date">
-      Thursday
-      </div>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Twemoji_1f346.svg/1200px-Twemoji_1f346.svg.png" 
-      alt="Penis"
-      width="40"
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Twemoji_1f346.svg/1200px-Twemoji_1f346.svg.png"
+        alt="Penis"
+        width="40"
       />
       <div class="weather-forecast-temperature">
-       <span class="temp-max">666°</span>  
-       <span class="temp-min">69°</span>
+        <span class="temp-max">666°</span>
+        <span class="temp-min">69°</span>
       </div>
-      <br>
     </div>
-   </div>
-  </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
-
-
-
 
 function displayWeatherCondition(response) {
   console.log(response.data);
