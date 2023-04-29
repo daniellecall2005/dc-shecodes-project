c8576a9d3daac6424f549b765d37b539f503beda
daniellecall2005 <124648834+daniellecall2005@users.noreply.github.com>
2023-04-29 16:30:37 -0600

    Before Integration

--- src/script.js
+++ src/script.js
@@ -54,7 +54,6 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "1819d5a44a288deaad951697832ebba0";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
