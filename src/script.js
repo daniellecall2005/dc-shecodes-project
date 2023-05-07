b02354c4e729da20054a5897226654f8da68ed84
daniellecall2005 <124648834+daniellecall2005@users.noreply.github.com>
2023-05-06 20:46:25 -0600

    Update script.js

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -48,12 +48,16 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+
+  console.log(response.data.daily);
 }
 
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "1819d5a44a288deaad951697832ebba0";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?
+  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -76,6 +80,8 @@ function displayWeatherCondition(response) {
   celsiusTemperature = response.data.main.temp;
 
   getForecast(response.data.coord);
+
+  console.log(response.data);
 }
 
 function search(city) {
